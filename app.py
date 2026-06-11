ce7f6ba7af6a4307ca1ce3fa76930f30b526df7f
Ryan-Wenstrom <ryan_wenstrom@berkeley.edu>
2026-06-10 17:25:54 -0700

    Adding steps 1-3?. hopefully

--- app.py
+++ app.py
@@ -1,26 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+app = Flask(__name__)
 import pandas as pd
 import os
+import sqlite3
 
-app = Flask(__name__)
 APP_FOLDER = os.path.dirname(os.path.realpath(__file__))
 
-@app.route("/")
-def hello():
+@app.route('/')
+def w209():
+    file='about9.jpg'
+    return render_template('w209.html',file=file)
+
+@app.route("/map")
+def map():
     return render_template("index.html")
 
 @app.route("/getData/<int:year>")
 def getData(year):
-    revenue = pd.read_csv(os.path.join(APP_FOLDER, "static/data/1_Revenues.csv"))
+    # Load the CSV file from the static folder, inside the current path
+    revenue = pd.read_csv(os.path.join(APP_FOLDER,"static/data/1_Revenues.csv"))
 
     if year < 1942 or year > 2008:
         return "Error in the year range"
 
-    filteredRevenue = revenue[revenue["Year4"] == year][
-        ["Name", "Year4", "Total Revenue", "Population (000)"]
-    ]
+    filteredRevenue = revenue[revenue['Year4']==year][["Name","Year4", "Total Revenue","Population (000)"]]
 
-    return filteredRevenue.to_json(orient="records")
+    # show the post with the given id, the id is an integer
+    return filteredRevenue.to_json(orient='records')
 
 
 @app.route("/api")
@@ -44,5 +50,6 @@ def get_nationality():
     result = cur.execute(f"SELECT nationality FROM players where short_name = '{player}'")
     return {"nationality": result.fetchone()[0]}
 
-if __name__ == "__main__":
+
+if __name__ == '__main__':
     app.run()
